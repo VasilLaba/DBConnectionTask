7d9655aca30b33ecdcbce0ee2181017691f71494
Vasil Laba <vasillaba1@gmail.com>
2021-09-29 15:14:02 +0300

    move crude operations to constants in CommentRepository

--- src/main/java/com/advertisement/project/repository/CommentRepository.java
+++ src/main/java/com/advertisement/project/repository/CommentRepository.java
@@ -12,10 +12,18 @@ import java.util.List;
 
 public class CommentRepository {
 
+    private static final String CREATE_SQL = "INSERT INTO comments (created_at, text, user_id, advertisement_id) VALUES (?,?,?,?);";
+    private static final String DELETE_SQL = "delete from comments WHERE id=?";
+    private static final String UPDATE_SQL = "UPDATE comments SET created_at,  text = ?  WHERE id = ?;";
+    private static final String SELECT_SQL = "SELECT * FROM advertisement;";
+    private static final String SELECT_BY_ID_SQL = "SELECT * FROM Comments JOIN users ON user_id  = users.id  WHERE  advertisement_id  = (?)";
+    private static final String SELECT_BY_USER_ID_SQL ="SELECT Id, title, created_at , description FROM Advertisements WHERE AuthorId = (?)";
+
+
+
     public static boolean setComment(Comment comment) throws IOException, SQLException {
         final Connection connection = DBConnector.getConnection();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(
-                "INSERT INTO comments (created_at, text, user_id, advertisement_id) VALUES (?,?,?,?);")) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(CREATE_SQL)) {
             preparedStatement.setDate(1, Date.valueOf(LocalDate.now()) /*Date.valueOf( advert.getDateOfCreating())*/);
             preparedStatement.setString(2, comment.getText());
             preparedStatement.setLong(3, comment.getUser().getId());
@@ -31,7 +39,7 @@ public class CommentRepository {
 
     public static boolean updateComment(Comment comment) throws IOException, SQLException {
         final Connection connection = DBConnector.getConnection();
-        try (PreparedStatement preparedStatement = connection.prepareStatement("UPDATE comments SET created_at,  text = ?  WHERE id = ?;")) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL)) {
             preparedStatement.setString(1, comment.getText());
             preparedStatement.setDate(2, Date.valueOf(LocalDate.now()));
             preparedStatement.setLong(3, comment.getId());
@@ -46,7 +54,7 @@ public class CommentRepository {
 
     public static boolean deleteComment(Long idDelete) throws IOException, SQLException {
         final Connection connection = DBConnector.getConnection();
-        try (PreparedStatement preparedStatement = connection.prepareStatement("delete from comments WHERE id=?")) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_SQL)) {
             preparedStatement.setLong(1,idDelete);
             preparedStatement.executeUpdate();
             return true;
@@ -59,8 +67,7 @@ public class CommentRepository {
         List<Comment> commentByUserIdList = new ArrayList<>();
         Comment comment;
         final Connection connection = DBConnector.getConnection();
-        try (java.sql.PreparedStatement statement = connection.prepareStatement("" +
-                "SELECT * FROM Comments JOIN users ON user_id  = users.id  WHERE  advertisement_id  = (?)"))
+        try (java.sql.PreparedStatement statement = connection.prepareStatement(SELECT_BY_ID_SQL))
         {
             statement.setLong(1, idSearch);
             final ResultSet resultSet = statement.executeQuery();
